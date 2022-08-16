fa30b71b40c30547c943f68beac993f3f8539b2a
mbence <mihaly.bence16@gmail.com>
2022-08-16 06:38:50 -0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by mihaly bence on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by mihaly bence on 16.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN")
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+            }
+            Button("HIT ME") {
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        ContentView().previewLayout(.fixed(width: 600, height: 300))
+    }
+}
+			
